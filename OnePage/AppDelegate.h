2c83a4b2909faf2cbb519321fe04a609e1d7a582
wenjing924wek <wenjing924wek@163.com>
2015-07-27 10:49:30 +0800

    Initial Commit

---
+++ OnePage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnePage
+//
+//  Created by lanouhn on 15/7/27.
+//  Copyright (c) 2015年 Lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OnePage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OnePage
+//
+//  Created by lanouhn on 15/7/27.
+//  Copyright (c) 2015年 Lanou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
